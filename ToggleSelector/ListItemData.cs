445e7dda6dd6fcbf05cc47163d76369a90458f1a
Lukas Hron <lukas.hron@ideastatica.com>
2023-12-13 22:23:26 +0100

    Static resource

---
+++ ToggleSelector/ListItemData.cs
@@ -0,0 +1,21 @@
+﻿namespace ToggleSelector
+{
+	public class ListItemData
+	{
+		public ListItemData()
+		{
+		}
+
+		public ListItemData(object value, object displayValue)
+		{
+			Value = value;
+			DisplayValue = displayValue;
+		}
+
+		public object Value { get; set; }
+
+		public object DisplayValue { get; set; }
+
+		public object ToolTip { get; set; }
+	}
+}
